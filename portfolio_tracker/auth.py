e3d2c70d212a6883c431cbfbb8bee5550ada4e54
Shaun Campbell <shaun.campbell3@gmail.com>
2025-09-23 18:57:07 -0500

    initial commit

---
+++ portfolio_tracker/auth.py
@@ -0,0 +1,104 @@
+import functools
+import re
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from portfolio_tracker.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = ''
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        else:
+            if not re.search(r"[a-z]", password):
+                error += 'Password must contain at lease one lowercase letter. '
+            if not re.search(r"[A-Z]", password):
+                error += 'Password must contain at lease one upercase letter. '
+            if not re.search(r"\d", password):
+                error += 'Password must contain at lease one number. '
+            if not re.search(r"[!@#$%^&*()_+-=\[\]{}|;:',.<>/?`~]", password):
+                error += 'Password must contain at lease special character. '
+            if len(password) < 8:
+                error += 'Password must be at least 8 characters long. '
+
+        if error == '':
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+                flash('Account created!')
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            flash('Login successful!')
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('main.index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('main.index'))
+
+        return view(**kwargs)
+
+    return wrapped_view
---
+++ portfolio_tracker/main.py
@@ -0,0 +1,14 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+
+from werkzeug.exceptions import abort
+
+from portfolio_tracker.auth import login_required
+from portfolio_tracker.db import get_db
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('main/index.html')
